6f199cd8a2f7f10c6afdb96f86b3384d27fd0ec8
Tyler Bair <tyler.bair@convergetechs.com>
2021-01-27 18:53:51 -0500

    more options added to test function

--- evmopcodes/test_bytecode.py
+++ evmopcodes/test_bytecode.py
@@ -2,10 +2,16 @@
 import os
 
 
-def trace_debug_bytecode(bytecode, sender=None):
+def trace_debug_bytecode(bytecode, sender=None, gas=None, price=None, data=None):
     command_str = "evm --debug --statdump --code " + bytecode
     if sender:
         command_str += " --sender " + sender
+    if gas:
+        command_str += " --gas " + gas
+    if price:
+        command_str += " --price " + price
+    if data:
+        command_str += " --input " + data
     command_str += " run"
     print(command_str)
     os.system(command_str)
